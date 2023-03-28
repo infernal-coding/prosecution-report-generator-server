8146b988887aec515dbde69131bd6335ef7478bd
Benjamin Eder <ben-eder@freenet.de>
2023-03-28 23:33:09 +0200

    doc: add documentation to status functions

--- web/status.go
+++ web/status.go
@@ -5,11 +5,13 @@ import (
 	"net/http"
 )
 
+// RegisterStatusEndpoints registers all endpoints for the status group on the given router group.
 func RegisterStatusEndpoints(group *gin.RouterGroup) {
 	v1 := group.Group("/v1/status")
-	v1.GET("/health", getStatusHealth)
+	v1.GET("/health", GetStatusHealth)
 }
 
-func getStatusHealth(c *gin.Context) {
+// GetStatusHealth provides an endpoint that responds with a status of 200 as the server is successfully running.
+func GetStatusHealth(c *gin.Context) {
 	c.String(http.StatusOK, "UP")
 }
